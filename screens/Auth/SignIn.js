bf0b95ec8ec443186142f629815af90d60472b62
kminny <darkdevil94@naver.com>
2021-04-05 16:37:02 +0900

    Finishing Sign In & Dismiss Keyboard

--- screens/Auth/SignIn.js
+++ screens/Auth/SignIn.js
@@ -1,32 +1,47 @@
 import React, { useState } from 'react';
+import { KeyboardAvoidingView, StatusBar } from 'react-native';
 import styled from 'styled-components';
 import Btn from '../../components/Auth/Btn';
 import Input from '../../components/Auth/Input';
+import DismissKeyboard from '../../components/DismissKeyboard';
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
+  align-items: center;
+`;
+
+const InputContainer = styled.View`
+  margin-bottom: 30px;
 `;
 
 export default () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const handelSubmit = () => alert(`${username} ${password}`);
+
   return (
-    <Container>
-      <Input
-        value={username}
-        placeholder="Username"
-        autoCapitalize="none"
-        stateFn={setUsername}
-      ></Input>
-      <Input
-        value={password}
-        placeholder="Password"
-        isPassword={true}
-        stateFn={setPassword}
-      ></Input>
-      <Btn text={'Sign In'} accent onPress={handelSubmit}></Btn>
-    </Container>
+    <DismissKeyboard>
+      <Container>
+        <StatusBar barStyle="dark-content"></StatusBar>
+        <KeyboardAvoidingView>
+          <InputContainer>
+            <Input
+              value={username}
+              placeholder="Username"
+              autoCapitalize="none"
+              stateFn={setUsername}
+            ></Input>
+            <Input
+              value={password}
+              placeholder="Password"
+              isPassword={true}
+              stateFn={setPassword}
+            ></Input>
+          </InputContainer>
+          <Btn text={'Sign In'} accent onPress={handelSubmit}></Btn>
+        </KeyboardAvoidingView>
+      </Container>
+    </DismissKeyboard>
   );
 };
--- screens/Auth/SignUp.js
+++ screens/Auth/SignUp.js
@@ -1,8 +1,61 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { KeyboardAvoidingView, StatusBar } from 'react-native';
+import styled from 'styled-components';
+import Btn from '../../components/Auth/Btn';
+import Input from '../../components/Auth/Input';
+import DismissKeyboard from '../../components/DismissKeyboard';
 
-export default () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Sign Up</Text>
-  </View>
-);
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const InputContainer = styled.View`
+  margin-bottom: 30px;
+`;
+
+export default () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handelSubmit = () => alert(`${username} ${password}`);
+
+  return (
+    <DismissKeyboard>
+      <Container>
+        <StatusBar barStyle="dark-content"></StatusBar>
+        <KeyboardAvoidingView behavior="position">
+          <InputContainer>
+            <Input
+              value={firstName}
+              placeholder="First Name"
+              autoCapitalize="none"
+              stateFn={setFirstName}
+            ></Input>
+            <Input
+              value={lastName}
+              placeholder="Last Name"
+              autoCapitalize="none"
+              stateFn={setLastName}
+            ></Input>
+            <Input
+              value={username}
+              placeholder="Username"
+              autoCapitalize="none"
+              stateFn={setUsername}
+            ></Input>
+            <Input
+              value={password}
+              placeholder="Password"
+              isPassword={true}
+              stateFn={setPassword}
+            ></Input>
+          </InputContainer>
+          <Btn text={'Sign Up'} accent onPress={handelSubmit}></Btn>
+        </KeyboardAvoidingView>
+      </Container>
+    </DismissKeyboard>
+  );
+};
