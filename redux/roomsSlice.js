25379c4ec82140d27e3034418fb0dfdd64603eca
kminny <darkdevil94@naver.com>
2021-04-09 02:53:14 +0900

    Updating fav view like real time

--- redux/roomsSlice.js
+++ redux/roomsSlice.js
@@ -33,10 +33,25 @@ const roomsSlice = createSlice({
     setFavs(state, action) {
       state.favs = action.payload;
     },
+    setFav(state, action) {
+      const {
+        payload: { roomId },
+      } = action;
+      const room = state.explore.rooms.find((room) => room.id === roomId);
+      if (room) {
+        if (room.is_fav) {
+          room.is_fav = false;
+          state.favs = state.favs.filter((room) => room.id !== roomId);
+        } else {
+          room.is_fav = true;
+          state.favs.push(room);
+        }
+      }
+    },
   },
 });
 
-export const { increasePage, setExploreRooms, setFavs } = roomsSlice.actions;
+export const { increasePage, setExploreRooms, setFavs, setFav } = roomsSlice.actions;
 
 export const getRooms = (page) => async (dispatch, getState) => {
   const {
