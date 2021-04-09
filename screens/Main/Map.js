256c9bceb31587f5ea9716443eda2db0d306d164
kminny <darkdevil94@naver.com>
2021-04-09 23:23:20 +0900

    MapView Card & Find Index by position

--- screens/Main/Map.js
+++ screens/Main/Map.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Dimensions, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 
@@ -25,21 +25,91 @@ const RoomContainer = styled.View`
 
 const RoomCard = styled.View`
   background-color: white;
-  width: ${width - 50};
-  height: 200px;
+  width: ${width - 50}px;
+  height: 120px;
+  border-radius: 10px;
+  padding: 0px 20px;
+  flex-direction: row;
+  align-items: center;
 `;
 
-const RoomName = styled.Text``;
+const RoomPhoto = styled.Image`
+  width: 80px;
+  height: 80px;
+  border-radius: 5px;
+  margin-right: 20px;
+`;
+
+const Column = styled.View`
+  width: 70%;
+`;
+
+const RoomName = styled.Text`
+  font-size: 18px;
+`;
+
+const RoomPrice = styled.Text`
+  font-size: 16px;
+  margin-top: 5px;
+`;
 
 const Map = ({ rooms }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const onScroll = (e) => {
+    const {
+      nativeEvent: {
+        contentOffset: { x },
+      },
+    } = e;
+    const position = Math.abs(Math.round(x / width));
+    setCurrentIndex(position);
+  };
   return (
     <Container>
-      <MapView style={StyleSheet.absoluteFill} />
-      <ScrollView showsHorizontalScrollIndicator={false} horizontal pagingEnabled>
+      <MapView
+        style={StyleSheet.absoluteFill}
+        camera={{
+          center: {
+            latitude: parseFloat(rooms[0].lat),
+            longitude: parseFloat(rooms[0].lng),
+          },
+          altitude: 700,
+          pitch: 0,
+          heading: 0,
+          zoom: 10,
+        }}
+      >
+        {rooms?.map((room) => (
+          <Marker
+            key={room.id}
+            coordinate={{
+              latitude: parseFloat(room.lat),
+              longitude: parseFloat(room.lng),
+            }}
+          />
+        ))}
+      </MapView>
+      <ScrollView
+        scrollEventThrottle={50}
+        onScroll={onScroll}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        pagingEnabled
+      >
         {rooms?.map((room) => (
           <RoomContainer key={room.key}>
             <RoomCard>
-              <RoomName>{room.name}</RoomName>
+              <RoomPhoto
+                source={
+                  room.photos[0]?.file
+                    ? { uri: room.photos[0]?.file }
+                    : require('../../assets/roomDefault.jpeg')
+                }
+              />
+              <Column>
+                <RoomName>{room.name}</RoomName>
+                <RoomPrice>${room.price}</RoomPrice>
+              </Column>
             </RoomCard>
           </RoomContainer>
         ))}
