6bd6f32311a75720b5b73d513a634b37e95d1fbf
kminny <darkdevil94@naver.com>
2021-04-09 18:48:37 +0900

    Map View Card

--- screens/Main/Map.js
+++ screens/Main/Map.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Dimensions, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { connect } from 'react-redux';
 import styled from 'styled-components';
+
+const { width } = Dimensions.get('screen');
 
 const Container = styled.View`
   flex: 1;
@@ -7,10 +12,44 @@ const Container = styled.View`
   justify-content: center;
 `;
 
-const Text = styled.Text``;
+const ScrollView = styled.ScrollView`
+  position: absolute;
+  bottom: 50px;
+`;
 
-export default () => (
-  <Container>
-    <Text>Map</Text>
-  </Container>
-);
+const RoomContainer = styled.View`
+  background-color: transparent;
+  width: ${width};
+  align-items: center;
+`;
+
+const RoomCard = styled.View`
+  background-color: white;
+  width: ${width - 50};
+  height: 200px;
+`;
+
+const RoomName = styled.Text``;
+
+const Map = ({ rooms }) => {
+  return (
+    <Container>
+      <MapView style={StyleSheet.absoluteFill} />
+      <ScrollView showsHorizontalScrollIndicator={false} horizontal pagingEnabled>
+        {rooms?.map((room) => (
+          <RoomContainer key={room.key}>
+            <RoomCard>
+              <RoomName>{room.name}</RoomName>
+            </RoomCard>
+          </RoomContainer>
+        ))}
+      </ScrollView>
+    </Container>
+  );
+};
+
+function mapStateToProps(state) {
+  return { rooms: state.roomsReducer.explore.rooms };
+}
+
+export default connect(mapStateToProps)(Map);
